627a3eb7153851a214614dec8b03a5dc8ddab799
Matheus Vitor Sousa Almeida <matheus.almeida@trf1.jus.br>
2024-06-03 15:45:26 -0300

    inicio do projeto

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import { Router, Request, Response } from "express";
+import project from '../package.json';
+// import * as appRoutes from '@/infra/routes';
+
+const routes = Router();
+
+routes.get('/', (request: Request, response: Response) => {
+	return response.status(200).send({
+		name: 'Pointer',
+		version: project.version
+	});
+});
+
+// routes.use(Object.values(appRoutes))
+
+
+export { routes }
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+// import helmet from "helmet";
+import 'dotenv/config';
+import { routes } from "@/routes";
+import { PORT } from "@/infra/config/environment";
+// import { middlewares } from "@/infra/middlewares";
+
+const app = express();
+
+// app.use(helmet());
+// app.use(middlewares);
+app.use(routes);
+
+if(process.env.NODE_ENV !== "tests") {
+	app.listen(PORT || 8000, function (){
+		console.info("Pointer running on port %d", PORT || 8000);
+	});
+}
+
+export { app };
